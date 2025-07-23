ac54a064012c1a9678ba2ff45ac712a5ad3f7dc5
Jerry <15210723175@163.com>
2025-07-23 16:01:13 +0800

    grpo

--- OpenRLHF/examples/scripts/train_grpo_ray_hybrid_engine_agent.sh
+++ OpenRLHF/examples/scripts/train_grpo_ray_hybrid_engine_agent.sh
@@ -1,5 +1,15 @@
 set -x
 
+# 1台机器，8块A100
+
+# 这里num_nodes都设置为1 -- 我只有1台机器
+# 需要调整：nums_gpus_per_node & vLLm的并行度
+# 法一：手动拆分，明确各个角色的卡的数量
+# 法二：Hybrid Engine自动拆分所有模型的卡的数量【此处--colocate_all_models】
+#        这里Reference, Actor, Reward, Critic, vLLM全部放到8张卡上，自己进行动态调度
+
+# 【rollout number】--n_samples_per_prompt：每个 prompt 要采样的样本数（也就是每个 prompt 会生成多少条独立的轨迹）；
+# 【一个batch（更新一次）生成的rollout data】rollout_batch_size * n_samples_per_prompt = 128*8 = 1024
 python3 -m openrlhf.cli.train_ppo_ray \
    --ref_num_nodes 1 \
    --ref_num_gpus_per_node 8 \
@@ -8,7 +18,7 @@ python3 -m openrlhf.cli.train_ppo_ray \
    --vllm_num_engines 4 \
    --vllm_tensor_parallel_size 2 \
    --colocate_all_models \
-   --vllm_gpu_memory_utilization 0.6 \
+   --vllm_gpu_memory_utilization 0.7 \
    --init_kl_coef 1e-3 \
    --gamma 1.0 \
    --use_kl_loss \
